cd1950ac70b6cf1ebc05a64b10959d7d7f1e39b7
Erika Horros-Wallin <erika.horros@gmail.com>
2022-08-14 23:59:56 +0300

    Some weird changes im not sure just

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Button = ({handleClick, text}) => {
   return (
@@ -27,9 +27,12 @@ const Statistics = ({good,neutral,bad,all}) => {
       </p>
     )
   }
+
+  
   const average = (good-bad)/all
   const positive = (good/all)*100
   return(
+    
     <table>
       <tbody>
         <StatisticLine text="good" value = {good} />
@@ -45,41 +48,26 @@ const Statistics = ({good,neutral,bad,all}) => {
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good+neutral+bad
+  const t = [{v: 1}, {v: 2}, {v: 3}]
 
-  const voteGood = () => {
-    setGood(good+1)
-  }
-  const voteNeutral = () => {
-    setNeutral(neutral+1)
-  }
-  const voteBad = () => {
-    setBad(bad+1)
-  }
+ 
+ 
+ let h= t.map(o => o.v).sum
+   /*   let c= t.map(o => {o.v}).reduce((s, o) => s + o, 0) 
+      let e= t.map(o => o.v).reduce((s, o) => {s + o}, 0)
+         let f= t.reduce((s, o) => { s + o.v }, 0)
+           let g= t.map(o => o.v).reduce((s, o) => s + o.v, 0)  
 
+ */
+
+ 
+  console.log(h)
+
+  const [x, setX] = useState(1)
   return (
-    <div>
-      <h1> give feeback </h1>
-      <Button
-        handleClick={voteGood}
-        text='good'
-      />
-       <Button
-        handleClick={voteNeutral}
-        text='neutral'
-      />
-       <Button
-        handleClick={voteBad}
-        text='bad'
-      />
-       <h1> statistics </h1>
-       <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-    </div>
+    <button onClick={null}>
+      press
+    </button>
   )
 }
-
 export default App
--- tyoaika.md
+++ tyoaika.md
@@ -3,13 +3,13 @@ Osat | Tunnit |
 Osa0 | 0.5 |
 Osa1 | 4 |
 Osa2 | 12 |
-Osa3 |9(kesken)|
-Osa4 |11(kesken)|
-Osa5 |13(kesken)|
-Osa6 |8(kesken)|
-Osa7 |18(kesken)|
-Osa8 | - |
-Osa9 | - |
+Osa3 | 12 |
+Osa4 | 12 |
+Osa5 | 14 |
+Osa6 | 10 |
+Osa7 | 22 |
+Osa8 | 6 kesken|
+Osa9 | 12 kesken |
 Osa10 | - |
 Osa11 | - |
 Osa12 | - |
