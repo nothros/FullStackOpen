7b7c67746880af9aba3304f98c718df555046eca
Erika Horros-Wallin <erika.horros@gmail.com>
2023-03-06 23:48:18 +0200

    part 13.1-13.3

---
+++ osa13/commands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs(
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes int DEFAULt 0
+);
+
+insert into blogs (author, url, title) values ('Dan Abramov', 'www.test.test', 'Writing Resilient Components')
+insert into blogs (author, url, title) values ('Martin Fowler', 'www.test.test', 'Is High Quality Software Worth the Cost?') 
+insert into blogs (author, url, title) values ('Robert C. Martin', 'www.test.test', 'FP vs. OO List Processing') 
